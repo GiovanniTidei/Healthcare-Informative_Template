eebc763f424420f891cb0e20191ff30bf1455423
GiovanniTidei <60188364+GiovanniTidei@users.noreply.github.com>
2020-05-25 15:38:30 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+(function($) {
+    "use strict"; // Start of use strict
+
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top)
+                }, 1000, "easeInOutExpo");
+                return false;
+            }
+        }
+    });
+
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#sideNav'
+    });
+
+})(jQuery); // End of use strict
+
+
+
+$(document).ready(function() {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 50) {
+            $("#back-to-top").fadeIn();
+        } else {
+            $("#back-to-top").fadeOut();
+        }
+    });
+    // scroll body to 0px on click
+    $("#back-to-top").click(function() {
+        $("body,html").animate({
+                scrollTop: 0
+            },
+            400
+        );
+        return false;
+    });
+});
+
+// Domande Frequenti
+
+const accordionItemHeaders = document.querySelectorAll(
+    ".accordion-item-header"
+);
+
+accordionItemHeaders.forEach((accordionItemHeader) => {
+    accordionItemHeader.addEventListener("click", (event) => {
+        // Uncomment in case you only want to allow for the display of only one collapsed item at a time!
+
+        // const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+        // if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+        //   currentlyActiveAccordionItemHeader.classList.toggle("active");
+        //   currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+        // }
+
+        accordionItemHeader.classList.toggle("active");
+        const accordionItemBody = accordionItemHeader.nextElementSibling;
+        if (accordionItemHeader.classList.contains("active")) {
+            accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+        } else {
+            accordionItemBody.style.maxHeight = 0;
+        }
+    });
+});
